5a5dd4be8df5b43d84b51774fece80b4a8e7c811
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:28:54 +0700

    Membuka file input dan membaca isinya baris per baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,6 +29,25 @@ int main()
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
-    
+    // membuat obyek input file stream dalam mode membaca
+    ifstream infile;
+    // membuka file 
+    infile.open("contohfile.txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+         // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+   
       
 }
