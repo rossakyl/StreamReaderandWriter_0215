3a22c5b2e0b108ef04a9ab5be3faf306af05c51b
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:19:25 +0700

    Implementasi penulisan data ke file hingga pengguna memasukkan 'q'

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,4 +10,25 @@ int main()
 
     cout << " Masukan Nama file : ";
     cin >> NamaFile;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menujuk ke sebuauh nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+        break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+
+        
 }
