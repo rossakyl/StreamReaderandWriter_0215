33fe6886775f7070ab9aa407506d94e3479e77f7
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:18:42 +0700

    Tambahkan fitur input nama file dari pengguna

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << " Masukan Nama file : ";
+    cin >> NamaFile;
+}
