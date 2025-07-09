fc1e88580550bdfad7c66e858ae9e3d2d7e4911f
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:13:43 +0700

    Inisialisasi array data dengan 3 elemen bertipe int

--- execptionStandard.cpp
+++ execptionStandard.cpp
@@ -3,3 +3,16 @@
 //membuat obyek exeception yang akan digunakan
 #include <array>
 //membuat obyek array yang akan digunakan
+using namespace std;
+
+int main(){
+    cout << "Awal Program" << endl; // penanda 1:....
+    try
+    {
+        array<int, 3> data = {10,20,30};
+        //pesan Array integer 3 elemen
+        cout << data.at(4) << endl;
+        //memanggil array elemen ke 5
+    }
+
+}
