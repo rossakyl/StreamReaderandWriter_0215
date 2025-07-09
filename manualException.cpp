11ad7cce50cd37dce03d3b41a8e9a7aac1bfb8f3
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:44:08 +0700

    Tambahkan output sambutan di awal program

---
+++ manualException.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        
+    }
+    
+}
