a3191b74670d9ac027de43e0ec8094930571f61a
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:14:13 +0700

    Tambahkan blok try-catch untuk menangani exception saat akses array di luar indeks

--- execptionStandard.cpp
+++ execptionStandard.cpp
@@ -14,5 +14,11 @@ int main(){
         cout << data.at(4) << endl;
         //memanggil array elemen ke 5
     }
-
+    catch(exception &e)
+    {
+      //penangkap menggunakan obyek exception
+      cout << e.what() << endl;
+      /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+    }
+    
 }
