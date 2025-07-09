3e530d8d4e405532ac19d43678115e33b91e8dae
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:27:06 +0700

    Inisialisasi library dan pembukaan file output untuk penulisan

---
+++ ioFile.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    // membuat obyek output file stream dalam mode menulis
+    ofstream outfile;
+    // membuka file
+    outfile.open("contohfile.txt");
+     
+}
