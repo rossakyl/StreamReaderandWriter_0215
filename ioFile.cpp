c1f6425159be9aef06a9f06e4e53db2e7d987c2c
rossakyl <rossakayla01@gmail.com>
2025-07-09 12:27:38 +0700

    Menambahkan loop input untuk menulis ke file hingga user memasukkan 'q'

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,5 +11,20 @@ int main()
     ofstream outfile;
     // membuka file
     outfile.open("contohfile.txt");
-     
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin,baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+        break;
+        // menulis dan memasukan nilai 'baris' ke dalam file
+        outfile << baris <<endl;
+    }
+    
 }
